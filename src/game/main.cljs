725f097f2c365f29db2fb24eb2a0a33af95f775d
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2018-11-18 22:32:15 +0800

    Trying random name generation algos

--- src/game/main.cljs
+++ src/game/main.cljs
@@ -1,10 +1,11 @@
 (ns game.main
   (:require [core.schema :as schema]
-            [clojure.spec.alpha :as spec]))
+            [clojure.spec.alpha :as spec]
+            [game.generate :as generate]))
 
 (defn create-random-cadet
   "Given some parameters, return a ap of cadet parameters"
   ([] (create-random-cadet {}))
   ([params]
-   (spec/conform ::schema/cadet {:name "something"
+   (spec/conform ::schema/cadet {:name (generate/random-name)
                                  :vocation :infantry})))
--- src/generate.cljs
+++ src/generate.cljs
@@ -1,2 +1,25 @@
 (ns generate
   (:require [clojure.spec.alpha :as spec]))
+
+(def vowel-phonemes
+  "English vowel-phonemes.
+  Taken from:
+  http://www.lancsngfl.ac.uk/curriculum/literacy/lit_site/lit_sites/phonemes_001/"
+  ["a" "e" "i" "o" "u" "ae" "ee" "ie" "oe" "ue" "oo" "ar" "ur" "or"
+   "au" "er" "ow" "oi" "air" "ear"])
+
+(def consonant-phonemes
+  "English consonant-phonemes.
+  Taken from:
+  http://www.lancsngfl.ac.uk/curriculum/literacy/lit_site/lit_sites/phonemes_001/"
+  ["b" "d" "f" "g" "h" "j" "k" "l" "m" "n" "p" "r" "s" "t" "v" "w"
+   "wh" "y" "z" "th" "ch" "sh" "zh" "ng"])
+
+(defn random-name
+  ([]
+   (random-name 5))
+  ([length]
+   "TODO: Determine what we have to do, and then implement it."
+   ;; This rand-nth repeatedly call might not work
+   [(take length (repeatedly #(rand-nth (interleave consonant-phonemes
+                                                    vowel-phonemes))))]))
